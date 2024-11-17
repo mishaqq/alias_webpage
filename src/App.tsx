0210c0fdd9271fcbb6ac79f4aba733e9536c3110
Mykhailo Danylenko <dron7432@gmail.com>
2024-11-17 14:31:35 +0100

    new phone mockup and some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -52,14 +52,14 @@ function App() {
         </nav>
 
         <section className="mt-20 flex flex-col lg:flex-row items-center gap-12">
-          <div className="flex-1 text-center lg:text-left">
+          <div className="flex-1 text-center lg:text-left ">
             <h1 className="text-5xl lg:text-7xl font-bold text-white mb-6 leading-tight tracking-wide">
               Аліас Українською
             </h1>
             <p className="text-white/80 text-xl mb-8 max-w-2xl">
               Напружте свої мізки, щоб пояснювати слова і фрази. Киньте виклик
               друзям та з'ясуйте хто тут мовний експерт. Але ні в якому разі не
-              використовуйте однокореневі слова.
+              використовуйте спільнокореневі слова.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
               <Button
@@ -70,7 +70,7 @@ function App() {
                 <svg viewBox="0 0 384 512" className="w-5 h-5 fill-current">
                   <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
                 </svg>
-                Скачати для iOS
+                Завантажити для iOS
               </Button>
               <Button
                 className="bg-black hover:bg-gray-900"
--- src/components/PhoneMockup.tsx
+++ src/components/PhoneMockup.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import Screenshot from "/assets/screenshot.png";
-export function PhoneMockup() {
+import Screenshot from "/assets/iphone15.png";
+
+export function PhoneMockup({ className }: { className?: string }) {
   return (
-    <div className="flex-1 relative">
-      <div className="relative mx-auto w-[280px] h-[580px] bg-black rounded-[3rem] border-[14px] border-gray-800 shadow-2xl transform rotate-12">
-        <img
-          src={Screenshot}
-          alt="Game Screenshot"
-          className="h-full w-full object-cover rounded-[2rem]"
-        />
-      </div>
-      <div className="absolute -top-20 -right-20 w-72 h-72 bg-[#2a549a] rounded-full filter blur-3xl opacity-20"></div>
-      <div className="absolute -bottom-20 -left-20 w-72 h-72 bg-[#2a549a] rounded-full filter blur-3xl opacity-20"></div>
+    <div className="relative w-5/6 h-5/6 lg:w-2/6 lg:h-2/6">
+      <img src={Screenshot} alt="Game Screenshot" className="relative z-10" />
+      <div className="absolute -top-20 -right-20 w-72 h-72 bg-[#2a549a] rounded-full filter blur-3xl opacity-30  z-0"></div>
+      <div className="absolute bottom-10  right-20 rotate-12 w-80 h-24 bg-[#2a549a] rounded-full filter blur-3xl opacity-100  z-0"></div>
     </div>
   );
 }
